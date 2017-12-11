86049141203175b4f29c13d78254eb39998b5bfb
EvelinaKuu <evelina.kuusinen@gmail.com>
2017-12-11 16:18:55 +0000

    Changed the event listener on start again button to display the level as 0 and call function generateQuestion instead of realoading the page.

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@
 $(() => {
   const $sentence = $('.sentence');
   const $options = $('.option'); // array of DOM elements (buttons)
-  // const $displayLevel = $('.$displayLevel');
+  const $displayLevel = $('.displayLevel');
   const $startAgain = $('.startAgain');
   let level = 0;
   let currentLevel = null;
@@ -46,10 +46,6 @@ $(() => {
   // how to increase level by one
   // console.log(sentences[level].correct);
   // document.getElementsByClass('sentences').innerHTML = sentences[0];
-  function addLevel() {
-    const numberOfLevel =
-  $sentence.html(currentQuestion);
-}
 
 
   $options.on('click', (event) => {
@@ -58,7 +54,9 @@ $(() => {
     // store the users choice inside a variable
     if (usersChoice === currentCorrect) {
       alert('Well done! Now translate the next word!');
-      // how do I add a level in the displayLevel class AND
+      level++;
+      // same as level = level + 1
+      $displayLevel.html(level);
       generateQuestion();
     } else {
       alert('Sorry, try again');
@@ -79,14 +77,14 @@ $(() => {
   // (sentences[level].options);
 
   //
-  $options.on('click', () => {
-    location.reload();
+  $startAgain.on('click', () => {
+    level = 0;
+    $displayLevel.html('0');
+    generateQuestion();
   }
 
   );
 });
-// i would probably have on object for sentences inside that object I´d have a sentence0, sentence1, sentence3, until sentence10. the key of those are the actual sentence in English, the key is the words that is the options like huevos and manzanas and something and the values are true, false and false. Make the button option display the key of the object 1 and make the div with the class sentences show the sentence.
-
 
 //
 // // In the DOM
